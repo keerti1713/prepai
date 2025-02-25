4565c04ccb300418938148768cfc2cba1658b4c5
Keerti Mishra <mishrakeerti1713@gmail.com>
2025-02-26 03:11:08 +0530

    Inngest updated

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,11 @@
 import { inngest } from "@/inngest/client";
 import { serve } from "inngest/next";
 import { CreateNewUser, GenerateNotes, GenerateStudyTypeContent} from "@/inngest/functions";
-
+export const runtime='edge';
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
+    streaming:'allow',
     functions: [
         CreateNewUser,
         GenerateNotes,
